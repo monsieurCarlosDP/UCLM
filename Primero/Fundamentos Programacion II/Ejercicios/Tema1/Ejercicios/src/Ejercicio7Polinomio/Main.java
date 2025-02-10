2e0e330043cfbd5000e0ddd01a0a5346c0f088f5
Carlos <cdperez.85@gmail.com>
2025-02-10 01:21:38 +0100

    Ejercicio Polinomio (a medio)

--- Primero/Fundamentos Programacion II/Ejercicios/Tema1/Ejercicios/src/Ejercicio7Polinomio/Main.java
+++ Primero/Fundamentos Programacion II/Ejercicios/Tema1/Ejercicios/src/Ejercicio7Polinomio/Main.java
@@ -2,6 +2,22 @@ package Ejercicio7Polinomio;
 
 public class Main {
 
-    Polinomio polinomio = new Polinomio(new double[]{2, 1.4, 5.2, 9, 2});
+    public static void main(String[] args) {
+        Polinomio polinomio = new Polinomio(new double[]{2, 1.4, 5.2, 9, 2});
+
+        Monomio monomio = new Monomio(3,4);
+        Monomio monomio2 = new Monomio(5,8);
+        polinomio.anadirMonomio(monomio);
+        System.out.println(polinomio.toString());
+        polinomio.anadirMonomio(monomio2);
+        System.out.println(polinomio.toString());
+
+
+
+
+    }
+
+
+
 
 }
